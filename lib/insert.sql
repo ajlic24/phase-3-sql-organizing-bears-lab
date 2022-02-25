18becc9bab0d29a3faf35812fa6a6e29a6bfd80b
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-25 16:46:14 -0500

    commit tree ededbf926904dbd143d21e4ba8895925a97157a2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'blue', 'mad', 1),
+('Rowdy', 4, 'F', 'black', 'mad', 0),
+('Tabitha', 4, 'M', 'blue', 'happy' 1),
+('Sergeant Brown', 4, 'M', 'blue', 'happy' 1),
+('Melissa', 4, 'M', 'blue', 'happy' 1),
+('Grinch', 4, 'M', 'blue', 'happy' 1),
+('Wendy', 4, 'M', 'blue', 'happy' 1),
+(NULL, 4, 'M', 'blue', 'happy' 1);
